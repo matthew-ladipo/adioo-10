e910103231482e61621c1458eb1f7b740506cbca
matthew-ladipo <135835433+matthew-ladipo@users.noreply.github.com>
2023-09-01 01:34:17 +0100

    Update App.jsx
    Footer commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Navbar from './Navbar'
 import Section from './Section'
 import Section2 from './Section2'
-import Footer from './footer'
+import Footer from './Footer'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 
